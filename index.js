3c3262ed81f952cbca6e3596d0473c7a25330fb8
Tony Keiser <keisertony@gmail.com>
2023-01-25 18:05:40 -0800

    Reading files and directories

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 const fs = require('fs')
 
 fs.readdir(process.cwd(), (err, filenames) => {
@@ -5,5 +6,25 @@ fs.readdir(process.cwd(), (err, filenames) => {
     console.error(err)
   }
 
-  console.log('Filenames:', filenames)
+  const allStats = Array(filenames.length).fill(null)
+
+  for (let filename of filenames) {
+    const index = filenames.indexOf(filename)
+
+    fs.lstat(filename, (err, stats) => {
+      if (err) {
+        console.error(err)
+      }
+
+      allStats[index] = stats
+
+      const ready = allStats.every((stats) => !!stats)
+
+      if (ready) {
+        allStats.forEach((stats, index) => {
+          console.log(filenames[index], stats.isFile())
+        })
+      }
+    })
+  }
 })

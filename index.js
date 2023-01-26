807819ddaf210ed447f1e0c3b3c355837e3ce8c5
Tony Keiser <keisertony@gmail.com>
2023-01-26 08:11:07 -0800

    Promisify reading file vs directory

--- index.js
+++ index.js
@@ -1,30 +1,28 @@
 #!/usr/bin/env node
 const fs = require('fs')
 
-fs.readdir(process.cwd(), (err, filenames) => {
+fs.readdir(process.cwd(), async (err, filenames) => {
   if (err) {
     console.error(err)
   }
 
-  const allStats = Array(filenames.length).fill(null)
-
   for (let filename of filenames) {
-    const index = filenames.indexOf(filename)
-
-    fs.lstat(filename, (err, stats) => {
-      if (err) {
-        console.error(err)
-      }
-
-      allStats[index] = stats
-
-      const ready = allStats.every((stats) => !!stats)
-
-      if (ready) {
-        allStats.forEach((stats, index) => {
-          console.log(filenames[index], stats.isFile())
-        })
-      }
-    })
+    try {
+      const stats = await lstat(filename)
+      console.log(filename, 'Directory?:', stats.isDirectory())
+    } catch (err) {
+      console.error(err)
+    }
   }
 })
+
+const lstat = (filename) => {
+  return new Promise((resolve, reject) => {
+    fs.lstat(filename, (err, stats) => {
+      if (err) {
+        reject(err)
+      }
+      resolve(stats)
+    })
+  })
+}
